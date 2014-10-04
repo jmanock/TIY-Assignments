81d1ebc9ea0f5b46c79b8acf95784d6a99e5785d
Jon Manock <jonmanock@gmail.com>
2014-10-04 18:13:37 +0000

    New branch new project

---
+++ 08--Conway-Lite.js
@@ -0,0 +1,91 @@
+var assert = require('assert');
+function test(actual, expected, success){
+  if(success === undefined)
+    success = 'Winner';
+  assert.strictEqual(actual, expected);
+  console.log(success)
+}
+ var board = [
+  [false, true, false],
+  [false, true, false],
+  [false, true, false],
+];
+
+function conway(){
+  // Input state of the cell and neighbor cells
+  // Output the state of the cell and what happenes to the neighbors
+
+}
+
+function neighborsOf(board, x, y){
+  // Input board = x y cordinates
+  // Output list of neighbor cells
+var neighbors;
+  if (x === 0 && y === 0){
+    neighbors = [board[0][1], board[1][1], board[1][0]];
+    return neighbors;
+  }
+  if(x === 0 && y === 1){
+    neighbors = [board[0][0], board[0][2], board[1][0],
+                 board[1][1], board[1][2]];
+    return neighbors;
+  }
+  if(x === 0 && y === 2){
+    neighbors = [board[0][1], board[1][2], board[1][1]];
+    return neighbors;
+  }
+  if (x === 1 && y === 0){
+    neighbors = [board[0][0], board[0][1], board[1][1],
+                 board[2][1], board[2][0]];
+    return neighbors;
+  }
+  if(x === 1 && y === 1){
+    neighbors = [board[0][0], board[0][1], board[0][2],
+                 board[1][0], board[1][2], board[2][0],
+                 board[2][1], board[2][2]];
+    return neighbors;
+  }
+  if(x === 1 && y === 2){
+    neighbors = [board[0][2], board[0][1], board[1][1],
+                 board[2][1], board[2][2]];
+    return neighbors;
+  }
+  if (x === 2 && y === 0){
+    neighbors = [board[1][0], board[1][1], board[2][1]];
+    return neighbors;
+  }
+  if(x === 2 && y === 1){
+    neighbors = [board[1][0], board[1][1], board[1][2],
+                 board[2][0], board[2][2]];
+    return neighbors;
+  }
+  if(x === 2 && y === 2){
+    neighbors = [board[1][1], board[1][2], board[2][1]];
+    return neighbors;
+  }
+
+}
+
+function tick(){
+  // Input the board
+  // Output new board the next generation
+
+}
+
+ console.log('still working??')
+console.log(neighborsOf(board,1,2));
+console.log(neighborsOf(board,1,1));
+console.log(neighborsOf(board,0,1));
+console.log(neighborsOf(board,0,2));
+console.log(neighborsOf(board,1,0));
+console.log(neighborsOf(board,1,1));
+console.log(neighborsOf(board,1,2));
+console.log(neighborsOf(board,2,0));
+console.log(neighborsOf(board,2,1));
+console.log(neighborsOf(board,2,2));
+console.log(board[0][0]);
+console.log(board[0][1]);
+console.log(board[1][0]);
+console.log(board);
+console.log(board[1][2]);
+console
