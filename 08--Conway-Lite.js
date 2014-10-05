e26d41f7c0ae26345b6b2ad9e9ab2c692f77575e
Jon Manock <JonManock@gmail.com>
2014-10-05 17:50:32 -0400

    functions work

--- 08--Conway-Lite.js
+++ 08--Conway-Lite.js
@@ -6,52 +6,36 @@ function test(actual, expected, success){
   assert.strictEqual(actual, expected);
   console.log(success)
 }
- var board = [
-  [false, true, false],
-  [false, true, false],
-  [false, true, false],
+
+var board = [
+ [false, true, false],
+ [false, true, false],
+ [false, true, false],
 ];
 
-function conway(board, x, y){
-  // Input state of the cell and neighbor cells
-  // Output the state of the cell and what happenes to the neighbors
+var board1 = [
+  [false, false, false],
+  [true, true, true],
+  [false, false, false],
+];
 
-  var cell = [x,y];
-  var neighbor = neighborsOf(board,x,y);
-  var liveNeighbor = 0;
-  var status = board[x][y];
+var board2 = [
+  [false, false, false],
+  [false, false, false],
+  [false, false, false],
+];
 
-  for(var i = 0; i < neighbor.length; i++){
-    if (neighbor[i] == true){
-      liveNeighbor++;
-    }
-  }
-  console.log('Cell ' + cell + ' is '+ status +' and has '
-              + liveNeighbor +  ' live neighbors.');
-  // Rule #1
-  if(liveNeighbor < 2){
-    liveNeighbor = false;
-  }
-  // Rule #2
-  if(liveNeighbor === 2){
-    liveNeighbor = true;
-  }
-  // Rule #3
-  if(liveNeighbor >3){
-    liveNeighbor = false;
-  }
-  // Rule #4
-  if(status === false && liveNeighbor === 3){
-    status = true;
-    liveNeighbors = false;
-  }
-  return ('The cell is '+ status +' and the neighbors are '+ liveNeighbor +'.');
- }
+var board3 = [
+  [true, true, true],
+  [true, true, true],
+  [true, true, true],
+];
 
 function neighborsOf(board, x, y){
-  // Input board = x y cordinates
+  // Input board = x y coordinates
   // Output list of neighbor cells
 var neighbors;
+
   if (x === 0 && y === 0){
     neighbors = [board[0][1], board[1][1], board[1][0]];
     return neighbors;
@@ -97,25 +81,79 @@ var neighbors;
 
 }
 
-function tick(){
-  // Input the board
-  // Output new board the next generation
+function conway(board,x,y){
+  // Input: Cell
+  // Output: Neighbors and cell with rules applied
+  var livingCell = 0;
+  var neighbors = neighborsOf(board,x,y);
+  var newCell;
+  var status = board[x][y];
 
+  for(var i = 0; i < neighbors.length; i++){
+    if(neighbors[i] === true){
+      livingCell++;
+    }
+  }
+    if (status === true){
+      // Rule #1
+      if (livingCell < 2) {
+        newCell = false;
+      }
+      // Rule #2
+      if (livingCell === 2 || livingCell === 3) {
+        newCell = true;
+      }
+      // Rule #3
+      if (livingCell > 3){
+        newCell = false;
+      }
+      // Rule #4
+      } else {
+        if (livingCell === 3){
+          newCell = true;
+        } else {
+          newCell = false;
+        }
+      }
+       return newCell;
 
 }
 
-// function newBoard(){
-//    return [
-//         [ false, false, false ],
-//         [ false, false, false ],
-//         [ false, false, false ],
-//     ];
-// }
+function tick(board){
+  // Input: board
+  // Output: newBoard
+  var newBoard = new Array(3);
+  console.log(board);
+  console.log('***************************')
+  for (var i = 0; i<board.length; i++){
+    newBoard[i] = new Array(3);
+    for (var j = 0; j<board[i].length; j++){
+      newBoard[i][j]=conway(board,i,j)
+      // newBoard.push = ([i,j]);
+      // newBoard.push = ([i,j]);
+      // newBoard.push = ([i,j]);
+      // console.log(newBoard,[i,j]);
+    }
+  }
+  return newBoard;
+}
 
+ console.log(tick(board));
+// console.log(tick(board1));
+// console.log(tick(board2));
+// console.log(tick(board3));
 
- console.log('still working?x?')
-// console.log(neighborsOf(board,1,2));
-// console.log(neighborsOf(board,1,1));
+// console.log(conway(board,0,0));
+// console.log(conway(board,0,1));
+// console.log(conway(board,0,2));
+// console.log(conway(board,1,0));
+// console.log(conway(board,1,1));
+// console.log(conway(board,1,2));
+// console.log(conway(board,2,0));
+// console.log(conway(board,2,1));
+// console.log(conway(board,2,2));
+
+// console.log(neighborsOf(board,0,0));
 // console.log(neighborsOf(board,0,1));
 // console.log(neighborsOf(board,0,2));
 // console.log(neighborsOf(board,1,0));
@@ -124,10 +162,3 @@ function tick(){
 // console.log(neighborsOf(board,2,0));
 // console.log(neighborsOf(board,2,1));
 // console.log(neighborsOf(board,2,2));
-// console.log(board[0][0]);
-// console.log(board[0][1]);
-// console.log(board[1][0]);
-// console.log(board);
-// console.log(board[1][1]);
-console.log(conway(board,1,0));
-console.log(conway(board,1,1));
