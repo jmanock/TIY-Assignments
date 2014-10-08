72265db09b5a1e0cc53f4dd1829ef252686c77ea
Jon Manock <JonManock@gmail.com>
2014-10-07 22:57:05 -0400

    changed if statments

--- dojo-2014-10-1-check.js
+++ dojo-2014-10-1-check.js
@@ -37,55 +37,49 @@ function test(actual, expected, success){
  */
 
 
-function numbersToWords(num){
-var val = num.toString();
 
-  ones=["zero", "one", "two", "three", "four", "five",
-        "six", "seven", "eight", "nine", "ten", "eleven",
-        "twelve", "thirteen", "fourteen", "fivteen", " sixteen",
-        "seventeen", "eightteen", "nineteen", "twenty"];
+  var ones=["zero", "one", "two", "three", "four", "five",
+      "six", "seven", "eight", "nine", "ten", "eleven",
+      "twelve", "thirteen", "fourteen", "fivteen", " sixteen",
+      "seventeen", "eightteen", "nineteen", "twenty"];
 
-  tens = ["zero", "ten","twenty ","thirty ","forty ", "fifty ",
-          "sixty ", " seventy ", "eighty ","ninty "];
-  //hundreds = ["zero","one hundred", "two"];
-  numerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var tens = ["zero", "ten","twenty ","thirty ","forty ", "fifty ",
+        "sixty ", " seventy ", "eighty ","ninty "];
 
-    if(num < 20){
-      return ones[num];
+
+
+function toEnglish(value){
+
+  //var value = value.toFixed(2);
+  //var cents = value.slice(-2);
+  //var cents = (cents + ' /100s dollars');
+  var value = value.toFixed(2);
+      cents = value.slice(-2);
+  var dollars = value.slice(0, -3);
+
+  if (dollars.length === 1){
+    return ones[dollars] +' and '+ cents +' /100s dollars';
+  }
+  if(dollars.length === 2){
+    if (dollars % 10 === 0){
+      return tens[dollars[0]] + ' and ' + cents + ' /100s dollars';
     }
-    if (num < 100){
-      if (num % 10 === 0){
-        return tens[val[0]];
-      }{
-        return tens[val[0]] + ones[val[1]];
-      }
-    }
-    if (num < 1000){
-      if (num % 10 === 0){
-        return tens[val[0]];
-      }{
-        return ones[val[0]] + ' hundred and ' + tens[val[1]] + ones[val[2]];
-      }
-    }
-    // if (num < 1000){
-    //   if (num % 10 === 0){
-    //     return tens[var[0]];
-    //   }{
-    //     return ones[var[0]] + "hundred" + ones[var[1]];
-    //   }
-    // }
+    return tens[dollars[0]] + ones[dollars[1]] + ' and ' + cents + ' /100s dollars';
+  }
+  if(dollars.length === 3){
+    return ones[dollars[0]] + ' hundred ' + tens[dollars[1]] + ones[dollars[2]] +
+      ' and ' + cents + ' /100s dollars';
+  }
 }
-console.log(numbersToWords(0));
-console.log(numbersToWords(2));
-console.log(numbersToWords(4));
-console.log(numbersToWords(10));
-console.log(numbersToWords(22));
-console.log(numbersToWords(30));
-console.log(numbersToWords(99));
-console.log(numbersToWords(101));
-console.log(numbersToWords(123));
-console.log('it should take 1 and return "one"',
-numbersToWords(1)==="one");
 
-console.log('it should take 3 and return "three"',
-numbersToWords(3) === "three");
+console.log('working?xx');
+// console.log(toEnglish(1));
+// console.log(toEnglish(1.49));
+// console.log(toEnglish(2));
+// console.log(toEnglish(2.54));
+// console.log(toEnglish(30));
+// console.log(toEnglish(35.59));
+// console.log(toEnglish(54.23));
+// console.log(toEnglish(60));
+console.log(toEnglish(232.43));
+console.log(toEnglish(543.32));
