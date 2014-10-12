dd96d2e7e3d2a17f9baaface856f7a255650509e
Jon Manock <JonManock@gmail.com>
2014-10-12 14:02:31 -0400

    working on test cases with chai

--- CheckwritingDojo.js
+++ CheckwritingDojo.js
@@ -1,72 +1,75 @@
-// @jmanock and @nhare Checkwritting dojo
-//var assert = require('chai').assert;
+// Check Writing Dojo with objects
+var assert = require('chai').assert;
 
-var ones = {zero:0, one:1, two:2, three:3, four:4, five:5};
+var ones = ['zero', 'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+    'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen', 'twenty'];
 
-// function toEnglish(value){
-//     return ones[value];
-// }
+var tens = ['zero', 'ten', 'twenty', 'thirty', 'fourty', 'fifty',
+    'sixty', 'seventy', 'eighty', 'ninty'];
 
-// var words = { zero: 0, one: 1, two: 2 };
-
-// function fromEnglish(word){
-//     return words[word];
-//
-//     if ( word === 'zero' ){
-//         return words[word];
-//         // return words['zero']; // word === 'zero'
-//         // return 0; // words.zero === 0
-//     }
-//     if ( word === 'one' ){
-//         return words[word];
-//         return words['one']; // word === 'one'
-//         // return 1; // words.one === 1
-//     }
-//     if ( word === 'two' ){
-//         return words[word];
-//         // return words['two']; // word === 'two'
-//         // return 2; // words.two === 2
-//     }
-// }
-
-var checkWriting = {
-  toEnglish: function toEnglish(value){
-    return ones[value];
-  },
-  fromEnglish: function fromEnglish(word){
-    return one[word];
-  }
-};
+var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+      'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
 
-console.log(checkWriting.toEnglish(1));
 
-// describe('toEnglish()', function(){
-//     it('should convert "zero" through "five"', function(){
-//         assert.equal(checkWriting.toEnglish(0), 'zero');
-//         assert.equal(checkWriting.toEnglish(1), 'one');
-//         assert.equal(checkWriting.toEnglish(2), 'two');
-//         assert.equal(checkWriting.toEnglish(3), 'three');
-//         assert.equal(checkWriting.toEnglish(4), 'four');
-//         assert.equal(checkWriting.toEnglish(5), 'five');
-//     });
-// describe('toEnglish()', function()){
-//     it('should not understand 6 yet', function(){
-//         assert.isUndefined(toEnglish(6));
-//     });
-// }
+var CheckWriting = {
+  toEnglish: function(value){
+    var value = value.toFixed(2),
+        cents = value.slice(-2),
+        dollars = value.slice(0, -3),
+        end = ' and ' + cents + '/100s';
 
-// describe('fromEnglish()', function(){
-//     it('should convert 0 through 2', function(){
-//         assert.equal(fromEnglish('zero'), 0);
-//         assert.equal(fromEnglish('one'), 1);
-//         assert.equal(fromEnglish('two'), 2);
-//         // assert.equal(fromEnglish('three'), 3);
-//         // assert.equal(fromEnglish('four'), 4);
-//         // assert.equal(fromEnglish('five'), 5);
-//     });
-//
-//     it('should not understand 6 yet', function(){
-//         // assert.isUndefined(fromEnglish('six'));
-//     });
-// });
+    if (dollars.length === 1){
+      return ones[dollars] + end;
+      }
+    if (dollars.length === 2){
+      if (dollars % 10 === 0){
+        return tens[dollars[0]] + end;
+      } else {
+        return tens[dollars[0]]  + ones[dollars[1]] + end;
+      }
+    }
+    if(dollars.length === 3){
+      if(dollars % 100 === 0){
+        return ones[dollars[0]] + ' hundreds' + end;
+      }else if (dollars % 10 === 0){
+        return ones[dollars[0]] + ' hundreds' + tens[dollars[1]] + end;
+      }else if (dollars[2] < 2){
+        return ones[dollars[0]] + ' hundreds' + tens[dollars[2]] + ones[dollars[2]] +
+          end;
+      }else{
+        return ones[dollar[0]] + ' hundreds' + tens[dollars[1]] + ones[dollars[2]] +
+         end;
+      }
+    }
+    if(dollars.length === 4){
+      if(dollars % 1000 === 0){
+        return ones[dollars[0]] + ' thousand' + end;
+      }else if(dollars % 100 === 0){
+        return ones[dollars[0]] + ' thousand' + ones[dollars[1]] + ' hundreds' +
+          tens[dollars[2]] + end;
+      }else if(dollars % 10 === 0){
+        return ones[dollars[0]] + ' thousand' + ones[dollars[1]] + ' hundreds' +
+          tens[dollars[2]] + end;
+      }else if(dollars[2] < 2){
+        return ones[dollars[0]] + ' thousand' + ones[dollars[1]] + ' hundreds' +
+          tens[dollars[3]] + end;
+      }else{
+        return ones[dollars[0]] + ' thousand' + ones[dollars[1]] + ' hundreds' +
+          tens[dollar[2]] + ones[dollar[3]] + end;
+      }
+    }
+  } // End toEnglish
+}; // End CheckWriting
+
+// Test Code
+describe('CheckWriting.toEnglish()', function(){
+  it('should convert "0" to "zero"', function(){
+    assert.equal(CheckWriting.toEnglish(0), 'zero and 00/100s');
+  });
+  it('should conver "20" to "twenty"', function(){
+    assert.equal(CheckWriting.toEnglish(20), 'twenty and 00/100s');
+  });
+});
