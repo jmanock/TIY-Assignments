f93f2e71be2e919ae05388ada5f7fe48b07399c3
Jon Manock <JonManock@gmail.com>
2014-10-16 21:49:31 -0400

    chai test added

--- Dojos/conwaysGameOfLive.js
+++ Dojos/conwaysGameOfLive.js
@@ -1,84 +1,129 @@
-//var expect = require('chai').expect;
-var assert = require("assert");
-var game = {
-  board: undefined,
-  newBoard: function(){  },
-  rules: function(cell, neighbors){
-  var liveN = 0
-  neighborsAre.forEach(function(value, index){
-    if (value === true){
-      liveN++;
+var assert = require('chai').assert;
+// Declaring Board State
+var board = [
+  [false, false, false],
+  [false, false, false],
+  [false, false, false],
+];
+
+var board2 = [
+  [false, false, false],
+  [false, true, false],
+  [false, false, false],
+];
+
+var board3 = [
+  [false, true, false],
+  [false, true, false],
+  [false, true, false],
+];
+
+var board4 = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, true],
+];
+
+var cellState = false;
+var neighbors = [];
+
+var conway = {
+  neighborsOf: function (board,x,y){
+    if (x === 0 && y === 0){
+      neighbors = [board[0][1], board[1][0], board[1][1]];
     }
-    if(position === false){
-      if(liveN === 3){
-        newState = true;
-      }else{
-        newState = false;
-      }
+    if (x === 0 && y === 1){
+      neighbors = [board[0][0], board[1][0], board[1][1], board[1][2],
+      board[0][2]];
     }
-    if(position === true){
-      if(liveN < 2){
-        newState = false;
-      }
-      if(liveN > 3){
-        newState = false;
-      }
-      if(liveN === 2 || liveN === 3){
-        newState = true;
-      }
+    if (x === 0 && y === 2){
+      neighbors = [board[0][1], board[1][1], board[1][2]];
     }
-  });
+    if (x === 1 && y === 0){
+      neighbors = [board[0][0], board[0][1], board[2][0], board[2][1],
+      board[2][0]];
+    }
+    if (x === 1 && y === 1){
+      neighbors = [board[0][0], board[1][0], board[2][0], board[1][2],
+      board[0][2], board[2][1], board[0][1], board[2][2]];
+    }
+    if (x === 1 && y === 2){
+      neighbors = [board[0][1], board[0][2], board[1][1], board[2][1],
+      board[2][2]];
+    }
+    if (x === 2 && y === 0){
+      neighbors = [board[1][0], board[1][1], board[2][1]];
+    }
+    if (x === 2 && y === 1){
+      neighbors = [board[2][0], board[1][0], board[1][1], board[1][2],
+      board[2][2]];
+    }
+    if (x === 2 && y === 2){
+      neighbors = [board[2][1], board[1][1], board[1][2]];
+    }
+    return neighbors;
   },
-  neighborsOf: function(x,y){
-    var diffs = [-1, 0, 1],
-    neighbors = [ ];
-    diffs.forEach(function(dX){
-      diffs.forEach(function(dY){
-        if(this.board.length === 0)return;
-        if(dX === 0 && dY === 0) return;
-        if(((x + dX) < 0) || ((y + dY) > 2)) return;
-        if(((y + dY) < 0) || ((y + dY) > 2)) return;
-        neighbors.push(this.board[x + dX] [y + dY]);
+
+  rules: function(cell, neighbors){
+    var liveCells = 0;
+    neighbors.forEach(function(value, index){
+      if(value === true){
+        liveCells++;
+      }
+      if(cell === true){
+        if(liveCells < 2){
+          cellState = false;
+        }
+        else if(liveCells < 4){
+          cellState = true;
+        }
+        else if(liveCells > 3){
+          cellState = false;
+        }
+      } else {
+        if(liveCells === 3){
+          cellState = true;
+        } else{
+          cellState = false;
+        }
+      }
+    });
+    return cellState;
+  },
+  tick: function(board){
+    var newBoard = [];
+
+    board.forEach(function(value, index){
+      value.forEach(function(x,y){
+        newBoard.push(conway.rules(board[index][y], conway.neighborsOf(board, index, y)));
       });
     });
-    return neighbors;
-    },
-  tick: function(){
-    var after = this.board;
-  this.board.forEach(function(row, x){
-    row.forEach(function(cell,y){
-      after[x][y] = game.rules(cell, game.neighborsOf(x,y));
-    });
+    var row1 = newBoard.splice(0,3);
+    var row2 = newBoard.splice(0,3);
+    board = [row1, row2, newBoard];
+    return board;
+  }
+}
+
+neighbors.forEach(conway.rules);
+
+describe('conway.neighborsOf', function(){
+  it('should return an array of neighbor values given a cell', function(){
+    assert.deepEqual(conway.neighborsOf(board, 0, 0), ([board[0][1], board[1][0], board[1][1]]));
   });
-    return after;
-  },
-  display: function(){
-    var spacer = '+------+-----+----\n';
+});
 
-    return spacer +
-    // Apply 'renderRow' to each 'row' in boad
-    this.board.map(function render(row){
-      return '|' + row.map(function renderCell(cell){
-        // return 'X' if cell is TRUTHY otherwise return
-        return cell && 'X' || ' ';
-      }).join('|') // Place '|' between each 'cell'
-      + '|\n';
-    }).join(spacer) // Place spacer between each row
-    + spacer;
-  } // End display
-}; // End game;
-
-// describe('Functions are in fact functions', function(){
-//   it('should have a function named newBoard', function(){
-//     assert(game.newBoard);
-//     assert(game.rules);
-//     assert(game.neighborsOf);
-//     assert(game.tick);
-//   });
-// });
-// describe('rules function works', function(){
-//   it('should ', function(){
-//     assert.equal(game.rules(true),[true,true,true])
-//   });
-// });
-console.log(game.display(game.tick(game.newBoard)));
+describe('conway.conway', function(){
+  it('should return the value of a cell based on its neighbors', function(){
+    assert.equal(conway.rules(board[0][0], [board[0][1], board[1][0], board[1][1]]), false);
+  });
+});
+describe('conway.tick', function(){
+  it('should return a new board after applying rules from conway', function(){
+    assert.deepEqual(conway.tick([
+      [false, false, false],
+      [false, false, false],
+      [false, false, false],
+      ]), conway.tick(board2));
+  });
+});
