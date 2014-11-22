a29d549c5d909dc87ee9b5cefd5f72d6a6929f90
Jon Manock <jonmanock@gmail.com>
2014-11-21 23:39:17 -0500

    removed files

--- src/app/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('tiyAssignments', ['ngCookies', 'restangular', 'ui.router']);
--- src/components/navbar/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('tiyAssignments')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
