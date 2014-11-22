cf467829b18f9b22c44abea19347a086abca73f8
Jon Manock <jonmanock@gmail.com>
2014-11-21 23:55:23 -0500

    Starting

---
+++ AngularizeGitHub/src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('angularizeGitHub', ['ngCookies', 'restangular', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
